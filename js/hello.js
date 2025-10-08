cd2627b11312ebc61d813d1a134c385fb9c603a9
Vhantera <jmerced17@gmail.com>
2025-10-07 22:25:23 -0400

    Initial scaffold

---
+++ js/hello.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    console.log("hello " + name + "!");
+}
+greet("joey");
+greet("Erin"); 
+greet("evie");
+
+console.log("I need to learn javascript in order to survive"); 
+
